9d27bc6733b9f9511c43675840419967017bd695
josuecama <37041807+Josue-la-cama@users.noreply.github.com>
2026-06-11 19:56:52 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1781221764,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"jquery.mobile.flatui.css",
+		"Flat-UI-Icons-24.ttf",
+		"Flat-UI-Icons-24.woff",
+		"lato-black.ttf",
+		"lato-black.woff",
+		"lato-bold.ttf",
+		"lato-bold.woff",
+		"lato-italic.ttf",
+		"lato-italic.woff",
+		"lato-regular.ttf",
+		"lato-regular.woff",
+		"ajax-loader.gif"
+	]
+}
