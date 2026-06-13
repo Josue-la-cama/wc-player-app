f2f9e9db1ef75118b30f2136f34e30eda60c7924
josuecama <37041807+Josue-la-cama@users.noreply.github.com>
2026-06-13 02:57:14 -0400

    v0.3 upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1781269270,
+	"version": 1781333778,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -8,6 +8,10 @@
 		"sprite-sheet0.png",
 		"sprite2-sheet0.png",
 		"sprite3-sheet0.png",
+		"9patch.png",
+		"sprite4-sheet0.png",
+		"sprite5-sheet0.png",
+		"sprite6-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
